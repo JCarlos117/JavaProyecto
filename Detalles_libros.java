523d1df6cc7f17fd42704b02a654d168035ec33a
JCarlos117 <jacostaguzman368@gmail.com>
2023-08-31 11:55:36 -0600

    primera version java

---
+++ Detalles_libros.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Seccion_3;
+
+import java.util.Scanner;
+
+
+public class Detalles_libros {
+
+    public static void main(String args[]) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("escriba los siguientes detalles sobre el libro: ");
+
+        System.out.println("proporcione el titulo del libro:");
+        var titulo = entrada.nextLine();
+
+        System.out.println("proporcione el autor del libro:");
+        var autor = entrada.nextLine();
+
+        System.out.println(titulo + " fue escrito por " + autor);
+
+    }
+
+}
